8a401f476bba82806317ab56bda2412418b50caa
Aparna Gopal <59289012+aparnag7@users.noreply.github.com>
2021-08-06 23:32:52 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/database";
 
 <script src="https://www.gstatic.com/firebasejs/8.9.0/firebase-app.js"></script>  // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: "AIzaSyCJtawZg_HBK749DhJQH6R4VsJZtUv-AHo",
-    authDomain: "reactcrud-505be.firebaseapp.com",
-    databaseURL: "https://reactcrud-505be-default-rtdb.firebaseio.com",
-    projectId: "reactcrud-505be",
-    storageBucket: "reactcrud-505be.appspot.com",
-    messagingSenderId: "384076428775",
-    appId: "1:384076428775:web:adb94d58667c064845b942"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   // Initialize Firebase
   var fireDb = firebase.initializeApp(firebaseConfig);
